96cb332828cb1e4b5a3330a29513fbee91160205
ddibiasi <daniel.dibiasi@gmail.com>
2020-01-11 14:39:39 +0100

    [TASK] Added dark mode

--- web/public/js/dash.js
+++ web/public/js/dash.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     ctx = document.getElementById('dash').getContext('2d');
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
+    Chart.defaults.global.defaultFontColor = "#fff";
     loadData();
     setInterval(loadData, 10000);
 });
@@ -71,12 +72,12 @@ function initBarChart(ctx, teams, points) {
                 label: 'Total points per Team',
                 data: points,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
